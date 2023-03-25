222629a99bd5bfd697c72b6b8da3301b41f888d5
sethcondie <sethhaskellcondie@gmail.com>
2023-03-24 20:09:03 -0600

    Cleaned up the jscode.

--- code/jscode.js
+++ code/jscode.js
@@ -12,29 +12,23 @@ let userConfig = {
 
 let coolingDown = false;
 let resetSet;
-let fieldData;
 const MAX_TRIGGERS = 90;
 
 window.addEventListener('onWidgetLoad', function (obj) {
     const fieldData = obj.detail.fieldData;
     for (let i = 0; i <= MAX_TRIGGERS; i++) {
         let fieldTrigger = fieldData[`trigger${i}`];
-        let fieldImage = fieldData[`image${i}`];
-        let fieldSound = fieldData[`sound${i}`];
 
-        if (fieldTrigger == "" || fieldTrigger === undefined)
+        if (fieldTrigger === "" || fieldTrigger === undefined)
             continue;
 
         let inputTriggers = fieldTrigger.split(", ");
         for (let inputTrigger of inputTriggers) {
-            console.log(inputTrigger);
             let tempArray = [];
             tempArray.push({
                 file:fieldData[`image${i}`],
                 sound:fieldData[`sound${i}`]
             });
-            //if the object has no array then add it to the triggerObject
-            //else the object exists add another entry to it
             if (userConfig.triggers[inputTrigger] === undefined) {
                 userConfig.triggers[inputTrigger] = [tempArray];
             } else {
@@ -46,15 +40,9 @@ window.addEventListener('onWidgetLoad', function (obj) {
 
 
 window.addEventListener('onEventReceived', function (obj) {
-    // console.log(obj);
-    // console.log(userConfig);
-
-    //validate the object
-    //make sure that the event is a chat command
     if ((obj.detail.listener !== 'message'))
         return;
 
-    //parse the input into an array
     let data = obj.detail.event.data;
     let input = data.text;
     //remove all non letters
@@ -69,9 +57,8 @@ window.addEventListener('onEventReceived', function (obj) {
     let triggerArray;
     for (let word of words) {
         word = word.trim();
-        //check for case sensivitity
         let caseSensitive = userConfig.caseSensitive;
-        if (caseSensitive == "off") {
+        if (caseSensitive === "off") {
             word = word.toLowerCase();
         }
 
@@ -102,7 +89,6 @@ window.addEventListener('onEventReceived', function (obj) {
     let imageSound = fieldsArray['sound'];
     updateImage(imageFile, imageSound);
 
-    //include the timers for the cooldown and the reset
     if (commandCooldown > 0) {
         coolingDown = true;
         setTimeout(function(){ coolingDown = false;}, commandCooldown * 1000);
